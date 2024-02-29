4469f6955cc2a429aa219b02386bf0953f1b8255
spoonz14 <barrspencer@yahoo.ca>
2024-02-29 11:48:11 -0700

    initializing

---
+++ study-web-app/src/main/java/Gamerz/studywebapp/StudyWebAppApplication.java
@@ -0,0 +1,13 @@
+package Gamerz.studywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyWebAppApplication.class, args);
+	}
+
+}
---
+++ study-web-app/src/test/java/Gamerz/studywebapp/StudyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Gamerz.studywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
