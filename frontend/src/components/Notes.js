e16231bdfaf763669d8424c96d29d9f28c22ab18
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-07 20:53:28 -0700

    Setting up Notes.js file
    Notes.js will handle main functions of note feature

---
+++ frontend/src/components/Notes.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import NoteList from "./components/NoteList";
+import NoteList from "./components/NoteEdit";
+import NoteList from "./components/NewNoteForm";
+import NoteList from "./components/NavBar";
