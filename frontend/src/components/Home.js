c859ea0c1d48d9469f890108c3efed4479897941
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-07 23:15:49 -0700

    Finishing Home Page Styling
    Home page styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from "../axios-config";
+import studyImage from "../components/StudyGroup.jpg";
 
 const Home = () => {
   return (
-    <div className="Home-header">
-      <h1>Welcome to Studii!</h1>
+    <div className="Home-container">
+      <img src={studyImage} alt="studyImage" className="studyImage" />
+      <div className="Home-header">
+        <h1>Welcome to Studii</h1>
+        <p>The ultimate study tool</p>
+      </div>
     </div>
   );
 };
