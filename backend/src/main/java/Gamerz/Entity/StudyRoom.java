608db90e2bdc5deb86c4ae34936d764ea1dc42bd
spoonz14 <barrspencer@yahoo.ca>
2024-03-08 14:33:16 -0700

    Update StudyRoom.java

--- backend/src/main/java/Gamerz/Entity/StudyRoom.java
+++ backend/src/main/java/Gamerz/Entity/StudyRoom.java
@@ -10,7 +10,8 @@ import java.util.List;
 @Data
 public class StudyRoom {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "study_room_id_generator")
+    @SequenceGenerator(name = "study_room_id_generator", sequenceName = "study_room_seq", allocationSize = 1)
     private Long study_room_id;
 
     private String roomName;
