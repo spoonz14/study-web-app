919a3e7e17e92b983528dd32e6e0703817aa4fba
LaneL117 <laneleuschen@gmail.com>
2024-03-07 18:49:51 -0700

    Initial Chat Commit

---
+++ backend/src/main/java/Gamerz/studywebapp/config/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package Gamerz.studywebapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+
+}
