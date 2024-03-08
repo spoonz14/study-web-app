c1c08c08fae407f6e5ed1d96ad8d04fc3c028d93
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-07 20:38:21 -0700

    Made a List of notes
    Added a list of notes with an add note button

--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
 import axios from "../axios-config";
+import React from "react";
 
-const NoteList = () => {
+const NoteList = ({ notes, chooseNote, addNote, addNoteForm }) => {
   return (
-    <div>
-      <h1>This is NoteList</h1>
+    <div className="note-List">
+      <h2>Notes</h2>
+      {!addNoteForm && (
+        <div className="add-note-container">
+          <button onClick={addNote}>Add Note</button>
+        </div>
+      )}
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <button onClick={() => chooseNote(note)}>{note.title}</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
