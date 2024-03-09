4d93d7556d22a3731503ba6005d2ad022992adc5
spoonz14 <barrspencer@yahoo.ca>
2024-03-09 16:26:42 -0700

    updating navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -20,6 +20,9 @@ const NavBar = () => {
           <li>
             <Link to="/Notes">Notes</Link>
           </li>
+          <li>
+            <Link to="/Chat">Chat</Link>
+          </li>
         </ul>
       </nav>
     </div>
