2951b246af6e001cd2e8ed85ca8c19995c0f8e48
CheongXina <104945315+CheongXina@users.noreply.github.com>
2024-03-09 23:58:43 -0700

    Revert "Revert "frontend UI""
    This reverts commit b5560b72170853986ea4aa05c0b4c70438f176a8.

--- frontend/src/components/Timers.js
+++ frontend/src/components/Timers.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Timers() {
+  const [userID, setUserID] = useState('');
+  const [description, setDescription] = useState('');
+  const [priority, setPriority] = useState('');
+  const [category, setCategory] = useState('');
+
+
+
+
+  const fetchData = async () => {
+    try {
+      const requestBody = {
+        userID: Number(userID), // Convert userID to a number since the input returns a string
+        description: description,
+        category: category,
+        priority: Number(priority)
+      };
+
+      const response = await axios.post('http://localhost:8090/registerTimer', requestBody);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
-    <div className="timer-page">
-      <h1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Eget est lorem ipsum
-        dolor. In vitae turpis massa sed elementum tempus egestas sed sed. Enim
-        blandit volutpat maecenas volutpat blandit aliquam etiam erat. Venenatis
-        tellus in metus vulputate eu scelerisque felis. Nullam eget felis eget
-        nunc. Semper quis lectus nulla at volutpat diam ut venenatis t
-      </h1>
+    <div className="timerSetup">
+      
+      <div className='timerHeader'>Agenda Manager</div>
+      <br></br>
+      <div className='TimerDataContainer'>
+        <label>User ID:</label>
+          <input type="number" value={userID} onChange={e => setUserID(e.target.value)} />
+        <label>Description:</label>
+          <input type="text" value={description} onChange={e => setDescription(e.target.value)} />
+        <label>Category:</label>
+          <input type="text" value={category} onChange={e => setCategory(e.target.value)} />
+        <label>Priority:  </label>
+          <input type="text" value={priority} onChange={e => setPriority(e.target.value)} />
+        </div>
+      <button onClick={fetchData}>Add Reminder</button>
+      
     </div>
+    
   );
 }
 
