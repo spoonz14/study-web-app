faefc70a46bc36b3749ec001dc67da9abc5cf077
LaneL117 <laneleuschen@gmail.com>
2024-03-09 18:04:54 -0700

    moved controller

--- backend/src/main/java/Gamerz/Controllers/ChatController.java
+++ backend/src/main/java/Gamerz/Controllers/ChatController.java
@@ -1,5 +1,6 @@
-package Gamerz.chat;
+package Gamerz.Controllers;
 
+import Gamerz.chat.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
