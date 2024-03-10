05e2d8f67c559c2938af9c63695e84015e78c3f0
spoonz14 <barrspencer@yahoo.ca>
2024-03-10 14:34:34 -0600

    trying to fix errors

--- backend/src/main/java/Gamerz/Repository/StudyRoomRepository.java
+++ backend/src/main/java/Gamerz/Repository/StudyRoomRepository.java
@@ -1,7 +1,6 @@
 package Gamerz.Repository;
 
 import Gamerz.Entity.StudyRoom;
-import Gamerz.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
