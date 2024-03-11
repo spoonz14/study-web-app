a9112099346efae8fe5a8f3a14255bc57cc0843d
bkarimy <130744100+bkarimy@users.noreply.github.com>
2024-03-11 17:38:35 -0600

    changes

--- frontend/src/components/Catalog.js
+++ frontend/src/components/Catalog.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Catalog = () => {
-    return <div className= "catalog-view"></div>;
+    return <div className= "catalog-panel">
+        <h1>Study Rooms</h1>
+        <button >Group 1</button>
+      <button >Group 2</button>
+    </div>;
 };
 
 export default Catalog;
