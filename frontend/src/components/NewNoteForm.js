76daa44bbcbbdcb37d942aeef999d19b00273c4f
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-12 20:51:33 -0600

    comments

--- frontend/src/components/NewNoteForm.js
+++ frontend/src/components/NewNoteForm.js
@@ -2,23 +2,29 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const NewNoteForm = ({ onAddNote, addNoteForm, setAddNoteForm }) => {
+  //Variables to store new note Title and Description
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  // function to update the title state when input change is made
   const newFormTitle = (e) => {
     setTitle(e.target.value);
   };
 
+  // function to update the description state when textarea change is made
   const newFormDescription = (e) => {
     setDescription(e.target.value);
   };
 
+  // function to add a new note
   const handleAddNote = () => {
+    // check if the title and description fields aren't empty
     if (title.trim() !== "" && description.trim() !== "") {
       onAddNote({
         title,
         description,
       });
+      // Reset the title and description states
       setTitle("");
       setDescription("");
       // Close the form after adding the note
--- frontend/src/components/NoteEdit.js
+++ frontend/src/components/NoteEdit.js
@@ -1,48 +1,55 @@
-// NoteEdit.js
-
 import React from "react";
 import "./styles.css";
 
 const NoteEdit = ({ note, onSaveNote, onDeleteNote }) => {
+  // State to store the edited note
   const [editedNote, setEditedNote] = React.useState(note);
 
+  // Update the edited note state when the 'note' prop changes
   React.useEffect(() => {
     setEditedNote(note);
   }, [note]);
 
+  // function to edit the title of the note
   const editTitle = (e) => {
     setEditedNote({ ...editedNote, title: e.target.value });
   };
 
+  // function to edit the description of the note
   const editDescription = (e) => {
     setEditedNote({ ...editedNote, description: e.target.value }); // Add description to editedNote
   };
-
+  // Handler function to save the edited note
   const saveNote = () => {
+    // Check if the title or description is empty
     if (
       editedNote.title.trim() === "" ||
-      editedNote.description.trim() === "" // Ensure description is not empty
+      editedNote.description.trim() === ""
     ) {
       alert("Please provide a title, and description for this note.");
       return;
     }
+    // Call the onSaveNote function to save the edited note
+    onSaveNote(editedNote);
 
-    onSaveNote(editedNote); // Save edited note
-
+    // Reset the edited note state
     setEditedNote({
       title: "",
       description: "", // Reset description
     });
   };
 
+  // function to delete the note
   const deleteNote = () => {
     if (onDeleteNote) {
+      // Call onDeleteNote function to delete the note
       onDeleteNote(editedNote);
     } else {
       console.error("onDelete error");
     }
   };
 
+  // Render the note edit form
   if (!editedNote) {
     return null;
   }
