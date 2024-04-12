1be5d539077da5c62f8800340fa4497a5b279dd4
spoonz14 <130717880+spoonz14@users.noreply.github.com>
2024-04-12 16:12:04 -0600

    bugfixing

--- backend/src/main/java/Gamerz/Configuration/JwtUtils.java
+++ backend/src/main/java/Gamerz/Configuration/JwtUtils.java
@@ -1,29 +1,29 @@
-package Gamerz.Configuration;
-
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
-import java.util.Date;
-
-public class JwtUtils {
-    private static final String SECRET = "6fdc841e62ded107a3cbd42578aa94e7a5629250319877a06e37858d04a6ac3b";
-    private static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    public static String generateToken(String userId, String username, String role) {
-        return Jwts.builder()
-                .setSubject(username)
-                .claim("id", userId)
-                .claim("role", role)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SECRET)
-                .compact();
-    }
-    public static String extractUsername(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-}
+//package Gamerz.Configuration;
+//
+//
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//
+//import java.util.Date;
+//
+//public class JwtUtils {
+//    private static final String SECRET = "6fdc841e62ded107a3cbd42578aa94e7a5629250319877a06e37858d04a6ac3b";
+//    private static final long EXPIRATION_TIME = 864_000_000; // 10 days
+//    public static String generateToken(String userId, String username, String role) {
+//        return Jwts.builder()
+//                .setSubject(username)
+//                .claim("id", userId)
+//                .claim("role", role)
+//                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+//                .signWith(SignatureAlgorithm.HS512, SECRET)
+//                .compact();
+//    }
+//    public static String extractUsername(String token) {
+//        return Jwts.parser()
+//                .setSigningKey(SECRET)
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .getSubject();
+//    }
+//}
 
